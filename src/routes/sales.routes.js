cc66796f94b83249cacb5f01ad6f58f394e78992
Jordan Guimarães <jordanggomes@gmail.com>
2023-12-21 12:16:14 -0300

    Aplicando Autorização em Rotas Específicas

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -1,13 +1,15 @@
-const { Router } = require("express");
-const SalesController = require("../controllers/SalesController");
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const { Router } = require('express')
+const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization')
+const SalesController = require('../controllers/SalesController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
-const salesRoutes = Router();
+const salesRoutes = Router()
 
-const salesController = new SalesController();
+const salesController = new SalesController()
 
-salesRoutes.use(ensureAuthenticated);
+salesRoutes.use(ensureAuthenticated)
+salesRoutes.use(verifyUserAuthorization(['admin', 'sale']))
 
-salesRoutes.get("/", salesController.index);
+salesRoutes.get('/', salesController.index)
 
-module.exports = salesRoutes;
+module.exports = salesRoutes
